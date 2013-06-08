de1d20fa4c30d37c77774ece070e1d0f586ed0ab
Cyrus-Baumgarten <cdbaumg@gmail.com>
2013-06-07 22:18:06 -0700

    adds redirect to root path

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    
+    if user_signed_in?
+      redirect_to sketches_path
+    end
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rev1::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  
+  get 'static_pages/home'
+  
   root 'static_pages#home'
 
   # Example of regular route:
