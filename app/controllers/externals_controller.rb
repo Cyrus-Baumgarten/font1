2f98ed4d7b0ac3bcd265c0db3129465f5c1ed3f2
Cyrus-Baumgarten <cdbaumg@gmail.com>
2013-06-10 15:52:21 -0700

    externals listed and edited

--- app/controllers/externals_controller.rb
+++ app/controllers/externals_controller.rb
@@ -1,25 +1,26 @@
 class ExternalsController < ApplicationController
   
   def show
+     @external = External.find(params[:id])
+     @sketch = @external.sketch
   end
 
   def index
   end
 
-  def new
-  end
-
-  def create
-  end
-
   def edit
     @external = External.find(params[:id])
+    @sketch = @external.sketch
   end
 
   def update
+    @external = External.find(params[:id])
+    @external.update_attributes(params[:external])
+    redirect_to @external
   end
 
   def destroy
   end
+  
   
 end
